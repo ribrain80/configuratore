60738623a6a0862c75431b990efdd48cae8a94d9
Brambilla Riccardo <BrambillaRi@valueteam.com>
2017-02-03 16:20:12 +0100

    two.js implementation working, before tuning

--- resources/views/split/step3.blade.php
+++ resources/views/split/step3.blade.php
@@ -12,16 +12,16 @@
 
 					<div class="input-group input-group-lg col-lg-3">
 					  <span class="input-group-addon" id="basic-addon3">Lun</span>
-					  <input type="text" class="form-control" aria-describedby="basic-addon3" v-model.lazy="length">
+					  <input type="text" class="form-control" id="length" aria-describedby="basic-addon3" v-model.lazy="length" v-on:keyup="updateDrawer">
 					</div>
 
 				</div>
 
 				<div class="row">
 
-					<div class="input-group input-group-lg col-lg-2">
+					<div class="input-group input-group-lg col-lg-3">
 					  <span class="input-group-addon" id="basic-addon3">Larg</span>
-					  <input type="text" class="form-control" aria-describedby="basic-addon3" v-model.lazy="width">
+					  <input type="text" class="form-control" id="width" aria-describedby="basic-addon3" v-model.lazy="width" v-on:keyup="updateDrawer">
 					</div>
 
 				</div>
@@ -30,13 +30,13 @@
 
 					<div class="input-group input-group-lg col-lg-3">
 					  <span class="input-group-addon" id="basic-addon3">Prof</span>
-					  <input type="text" class="form-control" aria-describedby="basic-addon3" v-model.lazy="depth">
+					  <input type="text" class="form-control" id="depth" aria-describedby="basic-addon3" v-model.lazy="depth" v-on:keyup="updateShoulder">
 					</div>						
 
 				</div>
 			</div>
 
-			<div class="col-lg-3" id="animation" style="width: 300px; height: 300px;border: 1px solid #000;"></div>
+			<div class="col-lg-3" id="animation" style="width: 300px; height: 300px; border: 1px solid #000;"></div>
 		</div>
 
 		<div class="row">
@@ -45,28 +45,66 @@
 	</form>
 	
 	<script>
-		
+
 		var step2 = new Vue({
+
 		  el: '#drawer_dimensions',
+
 		  data: {
-		    length: 0,
-		    width: 0,
-		    depth: 0
+		    length: 100,
+		    width: 100,
+		    depth: 10,
+		    two: {},
+		    shouder: {},
+		    rect: {},
+		    hor_line: {}
 		  },
+
 		  methods: {
+
 		    inittwo: function () {
-		      var elem = document.getElementById('animation');
-		      var two = new Two({ width: 285, height: 200 }).appendTo(elem);
-		      var rect = two.makeRectangle(150, 70, 100, 100);
-		      rect.linewidth = 7;
-		      rect.stroke = '#1C75BC';
-			  rect.fill = 'rgba(0, 200, 255, 0.75)';
-			  two.play();
-		    }
+
+		      	var elem = document.getElementById('animation');
+
+			  	this.two = new Two({ width: 300, height: 300, autostart: true }).appendTo(elem);
+
+			  	this.rect = this.two.makeRectangle(this.two.width/2 - 30, this.two.height/2 -30, 100, 100);
+		      	this.rect.linewidth = 7;
+		      	this.rect.stroke = '#1C75BC';
+			  	this.rect.fill = 'rgba(0, 200, 255, 0.75)'; 
+
+			  	var line = this.two.makeLine(this.two.width/2 - 30, this.two.height/2 -60, this.two.width/2 +30, this.two.height/2 -60);
+			  	line.stroke = '#000000';
+
+
+
+			  	this.shoulder = this.two.makeRectangle(this.two.width/2 - 30,  this.two.height/2 + 100, 100, 30);
+		      	this.shoulder.linewidth = 2;
+		      	this.shoulder.stroke = '#1C75BC';
+			  	this.shoulder.fill = 'rgba(0, 200, 255, 0.75)'; 
+		    },
+
+		   	updateDrawer: function() {	
+		   		this.two.remove( this.rect );
+				this.rect = this.two.makeRectangle(this.two.width/2 - 30, this.two.height/2 -30, this.width, this.length );
+			    this.rect.linewidth = 7;
+			    this.rect.stroke = '#1C75BC';
+				this.rect.fill = 'rgba(0, 200, 255, 0.75)';
+		  	},
+
+		   	updateShoulder: function() {	
+		   		this.two.remove( this.shoulder );
+				this.shoulder = this.two.makeRectangle(this.two.width/2 - 30, this.two.height/2 + 100, this.depth, 30);
+			    this.shoulder.linewidth = 2;
+			    this.shoulder.stroke = '#1C75BC';
+				this.shoulder.fill = 'rgba(0, 200, 255, 0.75)';
+		  	},		  	
+
 		  },
+
 		  ready() {
   				this.inittwo()
-			}
+		  }
 		});
 
 	</script>
