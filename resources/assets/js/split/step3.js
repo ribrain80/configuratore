a664de7941b9a25143e5d0aedd99385399b117d1
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-06 18:10:57 +0100

    Wip modifica js per step3

--- resources/assets/js/split/step3.js
+++ resources/assets/js/split/step3.js
@@ -1,3 +1,4 @@
+
 var step2 = new Vue({
 
     el: '#drawer_dimensions',
@@ -12,51 +13,108 @@ var step2 = new Vue({
         rect: {},
         hor_text_rect: "",
         hor_line_rect: {},
-        vert_line_rect: {}
+        vert_text_rect: {},
+        vert_line_rect: {},
+        drawer_text: {}
     },
 
     methods: {
 
         inittwo: function () {
 
+            // # Canvas element
             var elem = document.getElementById('animation');
 
+            // # TWO Instance
             this.two = new Two({ width: 300, height: 300, autostart: true }).appendTo(elem);
 
-            this.rect = this.two.makeRectangle(100, 110, 100, 100);
+            // # Rectangle ( drawer )
+            this.rect = this.two.makeRectangle(50, 100, 100, 100);
             this.rect.linewidth = 7;
-            this.rect.stroke = '#1C75BC';
-            this.rect.fill = 'rgba(0, 200, 255, 0.75)';
+            this.rect.stroke = '#999999';
+            this.rect.translation.x += 20;
 
-            var line = this.two.makeLine(50, 30, 150, 30);
-            line.stroke = '#222';
+            // # Width line ( drawer )
+            this.hor_line_rect = this.two.makeLine(18, 30, 120, 30);
+            this.hor_line_rect.stroke = '#222';
 
-            var text = this.two.makeText(110, 100, 15, 'normal' );
-            text.size = 12;
-            text.stroke = "#222";
-            text.family = "Raleway";
+            // # Width text ( drawer )
+            this.hor_text_rect = this.two.makeText(100 + " cm", 70, 15, 'normal' );
+            this.hor_text_rect.size = 12;
+            this.hor_text_rect.stroke = "#222";
+            this.hor_text_rect.family = "Raleway";
 
+            // # Length line ( drawer )
+            this.vert_line_rect = this.two.makeLine(140, 50, 140, 150);
+            this.vert_line_rect.stroke = '#222';
 
+            // # Length text ( drawer )
+            this.vert_text_rect = this.two.makeText(100 + " cm", 160, 100, 'normal' );
+            this.vert_text_rect.size = 12;
+            this.vert_text_rect.stroke = "#222";
+            this.vert_text_rect.family = "Raleway";
+            this.vert_text_rect.rotation = Math.PI/2;
+
+            // # Rectangle ( Shoulder )
             this.shoulder = this.two.makeRectangle(100,  220, 100, 30);
             this.shoulder.linewidth = 2;
-            this.shoulder.stroke = '#1C75BC';
-            this.shoulder.fill = 'rgba(0, 200, 255, 0.75)';
+            this.shoulder.stroke = '#999999';
+
+            // # Drawer label
+            this.drawer_text = this.two.makeText("Cassetto", 70, 100, 'normal' );
+            this.drawer_text.size = 12;
+            this.drawer_text.stroke = "#222";
+            this.drawer_text.family = "Raleway";
+
         },
 
         updateDrawer: function() {
-            this.two.remove( this.rect );
-            this.rect = this.two.makeRectangle(100,110, this.width, this.length );
+
+            // Remove to redraw
+            this.two.remove( [ this.rect, this.hor_line_rect, this.hor_text_rect, this.drawer_text, this.vert_line_rect, this.vert_text_rect ] );
+
+            // # Rectangle ( drawer )
+            this.rect = this.two.makeRectangle(this.width/2,100, this.width, this.length );
             this.rect.linewidth = 7;
-            this.rect.stroke = '#1C75BC';
-            this.rect.fill = 'rgba(0, 200, 255, 0.75)';
+            this.rect.stroke = '#999999';
+            this.rect.translation.x += 20;
+            this.rect.translation.y = 50 + parseInt( this.length )/2;
+
+            // # Width line ( drawer )
+            this.hor_line_rect = this.two.makeLine(18, 30, parseInt(this.width) + parseInt( this.rect.linewidth ) + 15, 30);
+            this.hor_line_rect.stroke = '#222';
+
+            // # Width text ( drawer )
+            this.hor_text_rect = this.two.makeText( parseInt( this.width ), this.hor_line_rect.getBoundingClientRect().width/2 + 15, 15, 'normal' );
+            this.hor_text_rect.size = 12;
+            this.hor_text_rect.stroke = "#222";
+            this.hor_text_rect.family = "Raleway";
+
+            var rect_rightmost = this.rect.getBoundingClientRect().left + this.rect.getBoundingClientRect().width + 20;
+
+            // # Length line ( drawer )
+            this.vert_line_rect = this.two.makeLine(rect_rightmost, 50, rect_rightmost, 150);
+            this.vert_line_rect.stroke = '#222';
+
+            // # Length text ( drawer )
+            this.vert_text_rect = this.two.makeText(this.length + " cm", rect_rightmost + 10, 100, 'normal' );
+            this.vert_text_rect.size = 12;
+            this.vert_text_rect.stroke = "#222";
+            this.vert_text_rect.family = "Raleway";
+            this.vert_text_rect.rotation = Math.PI/2;
+
+            this.drawer_text = this.two.makeText("Cassetto", 70, 100, 'normal' );
+            this.drawer_text.size = 12;
+            this.drawer_text.stroke = "#222";
+            this.drawer_text.family = "Raleway";
         },
 
         updateShoulder: function() {
             this.two.remove( this.shoulder );
             this.shoulder = this.two.makeRectangle(100, 220, this.length, this.depth);
             this.shoulder.linewidth = 2;
-            this.shoulder.stroke = '#1C75BC';
-            this.shoulder.fill = 'rgba(0, 200, 255, 0.75)';
+            this.shoulder.stroke = '#999999';
+
         },
 
     },
