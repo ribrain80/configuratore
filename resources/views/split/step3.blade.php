76cc92e850d6ed6ccfd2b0421630bce9afa0122f
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-07 15:01:10 +0100

    Rimosso .lazy per evitare che l'evento non si propagasse correttamente

--- resources/views/split/step3.blade.php
+++ resources/views/split/step3.blade.php
@@ -5,38 +5,38 @@
 	<h2 lang="it">Dimensioni cassetto</h2>
 
 	<form action='{{ route( "split.step4" ) }}' method="POST" v-on:submit.prevent="checkChoice" id="drawertypes">
-
+		<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 		<div class="row" id="drawer_dimensions">
-			<div class="col-lg-6">
+			<div class="col-lg-8">
 				<div class="row">
 
-					<div class="input-group input-group-lg col-lg-12">
-						<span class="input-group-addon" id="basic-addon3" lang="it">Lunghezza</span>
-						<input type="text" class="form-control" id="length" aria-describedby="basic-addon3" v-model.lazy="length" v-on:keyup="updateDrawer">
+					<div class="input-group input-group-lg col-lg-3">
+					  <span class="input-group-addon" id="basic-addon1">Lun</span>
+					  <input tabindex="0" type="text" class="form-control" id="length" aria-describedby="basic-addon1" v-model="length" v-on:keyup="updateDrawer">
 					</div>
 
 				</div>
 
 				<div class="row">
 
-					<div class="input-group input-group-lg col-lg-12">
-						<span class="input-group-addon" id="basic-addon3" lang="it">Larghezza</span>
-						<input type="text" class="form-control" id="width" aria-describedby="basic-addon3" v-model.lazy="width" v-on:keyup="updateDrawer">
+					<div class="input-group input-group-lg col-lg-3">
+					  <span class="input-group-addon" id="basic-addon2">Larg</span>
+					  <input tabindex="1" type="text" class="form-control" id="width" aria-describedby="basic-addon2" v-model="width" v-on:keyup="updateDrawer">
 					</div>
 
 				</div>
 
 				<div class="row">
 
-					<div class="input-group input-group-lg col-lg-12">
-						<span class="input-group-addon" id="basic-addon3" lang="it">Profondità</span>
-						<input type="text" class="form-control" id="depth" aria-describedby="basic-addon3" v-model.lazy="depth" v-on:keyup="updateShoulder">
+					<div class="input-group input-group-lg col-lg-3">
+					  <span class="input-group-addon" id="basic-addon3">Prof</span>
+					  <input tabindex="2" type="text" class="form-control" id="depth" aria-describedby="basic-addon3" v-model="depth" v-on:keyup="updateShoulder">
 					</div>
 
 				</div>
 			</div>
 
-			<div class="col-lg-5 center-block" id="animation" style="width: 300px; height: 300px;"></div>
+			<div class="col-lg-3" id="animation" style="width: 300px; height: 300px; border: 1px solid #000;"></div>
 		</div>
 
 		<div class="row">
