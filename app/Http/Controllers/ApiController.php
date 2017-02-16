557b889e67eea46f773f2db30ea98b2d8ed829b6
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-16 10:29:42 +0100

    WIP: ApiController save/update model

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Divider;
+use App\Drawer;
 use App\Drawertype;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,49 @@ class ApiController extends Controller
 
     public function actionDividers() {
         return response()->json(Divider::all(['id','width','lenght','height'])->toArray());
+    }
+
+    public function actionSavedrawer(Request $request) {
+        //1 Recupero del json
+        $data = $request->json()->all();
+        //2 Inizializzo l'oggetto (sia save che update)
+        $drawer = $this->loadDrawerById($data->drawerId);
+        //3 Aggiorno i campi di model (fake)
+        $drawer->lenght=100;
+        $drawer->width=100;
+        $drawer->height=10;
+        $drawer->drawertypes_id=1;
+        //4 Gestisco i dividers ....
+        $dividers = [1,2,3];
+        $drawer->drawerdividers()->sync($dividers);
+        //5 Gestisco i bridges
+        $bridges = [];
+        $drawer->drawerbridges()->sync($bridges);
+        //2 Salvataggio
+        $saved = $drawer->save();
+
+        //3 Ritorno un oggetto in json con l'id del cassetto salvato in db
+        $out = [];
+        if ($saved) {
+            $out['status']='ok';
+            $out['id']=$saved->id;
+        } else {
+            $out['status']='ko';
+            $out['id']=-1;
+        }
+
+        return response()->json($out);
+    }
+
+
+    private function loadDrawerById($id=null) {
+        $drawer = null;
+        if (!$id) {
+            $drawer =  new Drawer();
+        } else {
+            $drawer = Drawer::findOrFail($id);
+        }
+        return $drawer;
     }
 
 
