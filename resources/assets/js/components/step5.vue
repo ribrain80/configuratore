acd6a82d3c672f64884763c56584e725ad5751bf
Brambilla Riccardo <BrambillaRi@valueteam.com>
2017-02-21 12:24:24 +0100

    step5 javascript checks initial import

--- resources/assets/js/components/step5.vue
+++ resources/assets/js/components/step5.vue
@@ -5,6 +5,12 @@
         <div class="col-lg-12">
             <h2 lang="it">Step finale</h2>
         </div>
+		
+		<div class="col-lg-12" v-show="$parent.has_error">
+            <div class="alert alert-danger alert-dismissible fade in"  lang="it">
+                <button type="button" class="close" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">×</span></button> <strong lang="it">Attenzione!</strong> {{ $parent.alert_message }}
+            </div>
+        </div>
 
 		<div class="checkbox col-lg-12">
 			<label>
@@ -22,7 +28,12 @@
 			<label>
 				<input type="text" v-model="$parent.email" /> Indirizzo email 
 			</label>
+		</div>
+		<div class="col-lg-12">
 			<button class="btn btn-danger" id="email" @click="$parent.savedrawer( $event )">Invia via email</button>
+		</div>
+		<div class="col-lg-12">
+			Oppure 
 		</div>
 
 		<div class="col-lg-12">	
--- resources/assets/js/split/step5.js
+++ resources/assets/js/split/step5.js
@@ -1,48 +1,89 @@
 var step5 = new Vue({
+
     el: 'step5',
+
     data: {
         brochure: false,
         summary: false,
         email: '',
         send: false,
-        download: false
+        download: false,
+        alert_message: '',
+        has_error: false
     },
+
     methods: {
+
         savedrawer: function() {
-          
-            if (event.target.id == "email") {
+            
+            if( this.brochure === false && this.summary == false ) {
+                this.has_error = true;
+                this.alert_message = "E' necessario scegliere almeno una delle opzioni tra Brochure e Riepilogo";
+                return false;
+            }
+
+            if ( event.target.id == "email" ) {
+
+                if( this.email.length === 0 ) {
+                    console.log( "IN" );
+                    this.has_error = true;
+                    this.alert_message = "E' necessario indicare un indirizzo email per la spedizione";
+                    return false;
+                }
+
+                if( !this.validateEmail() ) {
+                    this.has_error = true;
+                    this.alert_message = "l'email indicata non è valida";
+                    return false;
+                }                
+
                 this.send = true;
                 this.download = false;
             }
-            if (event.target.id == "download") {
+
+
+            if ( event.target.id == "download" ) {
                 this.send = false;
                 this.download = true;
             }
 
-            this.$http.post('/split/savedrawer', Configuration).then(response => {
+            this.$http.post( '/split/savedrawer', Configuration).then( response => {
                 console.log("success");
             }, response => {
-                console.log("something went wrong");
+                this.alert_message = "Impossibile completare l'operazione, si prega di riprovare più tardi";
+                this.has_error = true;
             });
+        },
+
+        validateEmail: function() {
+            var pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return pattern.test( this.email );
         }
     },
+
     watch: {
+
         brochure: function(val) {
             Configuration.pdf.brochure = val;
         },
+
         summary: function(val) {
             Configuration.pdf.summary = val;
         },
+
         email: function(val) {
             Configuration.pdf.email = val;
         },
+
         send: function( val ) {
           Configuration.pdf.send = val;
         },
+
         download: function( val ) {
           Configuration.pdf.download = val;
         }
     },
+
     mounted() { // # Window onload eq
         console.log("Step5 mounted!")
     }
