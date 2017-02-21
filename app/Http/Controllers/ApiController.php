ce6e0e180b8e4975a2d5036278081c76608c9fe9
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-21 10:58:53 +0100

    WIP GESTIONE ROTTE: Create le rotte per send e download pdf, creato un controller specifico, metodo unico di salvataggio da richiamare prima di fare send e pdf Aggiunta rotta per avere le configurazioni

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bridge;
 use App\Models\Divider;
 use App\Models\Drawer;
-use App\Models\Drawers;
 use App\Models\Drawertype;
 use Illuminate\Http\Request;
 use Log;
@@ -14,63 +14,21 @@ class ApiController extends Controller
     public function actionDrawersType() {
         return response()->json(Drawertype::all(['id','description'])->toArray());
     }
-
     public function actionDividers() {
         return response()->json(Divider::all(['id','width','length','depth'])->toArray());
     }
-
-    public function actionSavedrawer(Request $request) {
-        //TODO: Usare una transazione!!!
-        //1 Recupero del json
-        $data = $request->json()->all();
-        //2 Inizializzo l'oggetto (sia save che update)
-        $drawer = $this->loadDrawerById($data['drawerId']);
-        //3 Aggiorno i campi di model (fake)
-        $drawer->length=(int)$data['dimensions']['length'];
-        $drawer->width=(int)$data['dimensions']['width'];
-        $drawer->depth=(int)$data['dimensions']['depth'];
-        $drawer->drawertypes_id=$data['drawertype'];
-        $saved = false;
-        $error = "";
-        try {
-            $saved=$drawer->saveOrFail();
-            //4 Gestisco i dividers ....(PER ORA SOLO PER IL NOSTRO TEST!)
-            //TODO: CONTROLLARE CHE SUCCEDE SE RIMANE MA CAMBIO I CAMPI X/Y
-            $dividers=[];
-            foreach ($data['dividers'] as $divider) {
-                $dividers[$divider]=['x'=>0,'y'=>0];
-            }
-            $drawer->drawerdividers()->sync($dividers);
-            //5 TODO: Gestisco i bridges
-            $bridges = [];
-        } catch (\Exception $ex) {
-            $error=$ex->getMessage();
-        }
-
-        //6 Ritorno un oggetto in json con l'id del cassetto salvato in db
-        $out = [];
-        if ($saved) {
-            $out['status']='ok';
-            $out['id']=$drawer->id;
-        } else {
-            $out['status']='ko';
-            $out['id']=-1;
-        }
-
-        return response()->json($out,($saved)?200:400);
+    public function actionBridges() {
+        //TODO: Rename fields
+        return response()->json(Bridge::all(['id','width','lenght','height'])->toArray());
     }
 
+    public function actionConfig() {
+        $out['urls']['dividers']=route('split.api.drawerstypes');
+        $out['urls']['save']=route('split.api.savedrawer');
+        $out['urls']['send']=route('split.api.senddrawer');
+        $out['urls']['pdf']=route('split.api.pdfdrawer');
 
-    private function loadDrawerById($id=null) {
-        $drawer = null;
-        if (!$id) {
-            $drawer =  new Drawer();
-        } else {
-            $drawer = Drawer::findOrFail($id);
-        }
-        return $drawer;
+        return response()->json($out);
     }
-
-
 
 }
---
+++ app/Http/Controllers/SplitDrawerController.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidoff
+ * Date: 21/02/17
+ * Time: 10.31
+ */
+
+namespace App\Http\Controllers;
+
+use App\Models\Drawer;
+use Illuminate\Http\Request;
+
+class SplitDrawerController extends Controller
+{
+    public function actionSave(Request $request) {
+        $data = $request->json()->all();
+        $output = $this->save($data);
+        return response()->json($output,($output['id'!=-1])?200:400);
+    }
+
+    public function actionSend(Request $request) {
+        $data = $request->json()->all();
+        $output = $this->save($data);
+        return response()->json("DUMMY",200);
+    }
+
+    public function actionPdf(Request $request) {
+        $data = $request->json()->all();
+        $output = $this->save($data);
+        return response()->json("DUMMY",200);
+    }
+
+
+    private function save($data) {
+        //2 Inizializzo l'oggetto (sia save che update)
+        $drawer = $this->loadDrawerById($data['drawerId']);
+        //3 Aggiorno i campi di model (fake)
+        $drawer->length=(int)$data['dimensions']['length'];
+        $drawer->width=(int)$data['dimensions']['width'];
+        $drawer->depth=(int)$data['dimensions']['depth'];
+        $drawer->drawertypes_id=$data['drawertype'];
+        $saved = false;
+        $error = "";
+        try {
+            $saved=$drawer->saveOrFail();
+            //4 Gestisco i dividers ....(PER ORA SOLO PER IL NOSTRO TEST!)
+            //TODO: CONTROLLARE CHE SUCCEDE SE RIMANE MA CAMBIO I CAMPI X/Y
+            $dividers=[];
+            foreach ($data['dividers'] as $divider) {
+                $dividers[$divider]=['x'=>0,'y'=>0];
+            }
+            $drawer->drawerdividers()->sync($dividers);
+            //5 TODO: Gestisco i bridges
+            $bridges = [];
+        } catch (\Exception $ex) {
+            $error=$ex->getMessage();
+        }
+
+        //6 Ritorno un oggetto in json con l'id del cassetto salvato in db
+        $out = [];
+        if ($saved) {
+            $out['status']='ok';
+            $out['id']=$drawer->id;
+        } else {
+            $out['status']='ko';
+            $out['error']=$error;
+            $out['id']=-1;
+        }
+        return $out;
+    }
+
+    private function loadDrawerById($id=null) {
+        $drawer = null;
+        if (!$id) {
+            $drawer =  new Drawer();
+        } else {
+            $drawer = Drawer::findOrFail($id);
+        }
+        return $drawer;
+    }
+}
