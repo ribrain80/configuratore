16681bd78e5aaec16b7da4ae628d8098fed846f2
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-22 15:48:12 +0100

    WIP

--- resources/assets/js/components/step4.vue
+++ resources/assets/js/components/step4.vue
@@ -1,9 +1,7 @@
 <template>
-    <div id="step4">
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 lang="it">Selezione inserti</h2>
-            </div>
+    <div class="row" id="step4">
+        <div class="col-lg-12">
+            <h2 lang="it">Selezione inserti</h2>
         </div>
         <div class="row">
             <div id="container">
@@ -12,17 +10,37 @@
               <div id="box3" class="box">box3</div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-3" v-for="divider in $parent.dividers">
-                <div class="checkbox">
-                    <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}} x {{divider.depth}})</label>
+        -->
+        <div class="col-lg-12">
+            <ul class="nav nav-tabs">
+                <li  :class="{active: !index}" v-for="(cat,index) in $parent.dividers.dividersCategories">
+                    <a data-toggle="tab" :href="$parent.genHref(cat)"> Elem h-{{cat}}</a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div id="elem445" class="tab-pane fade in active">
+                    <div class="col-lg-3" v-for="divider in $parent.dividers.dividers">
+                        <div class="checkbox">
+                            <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}} x {{divider.depth}})</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="elem705" class="tab-pane fade in ">
+                    <div class="col-lg-3" v-for="divider in $parent.dividers">
+                        <div class="checkbox">
+                            <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}} x {{divider.depth}})</label>
+                        </div>
+                    </div>
                 </div>
 
             </div>
+        </div>
 
-            <div class="col-lg-12" >
-                <a href="#step5" class="btn btn-danger inpagenav" lang="it">Avanti</a>
-            </div>
+
+
+        <div class="col-lg-12" >
+            <a href="#step5" class="btn btn-danger inpagenav" lang="it">Avanti</a>
         </div>
     </div>
 </template>
