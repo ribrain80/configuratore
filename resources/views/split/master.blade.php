71f3f83732daf1b0ddec1568a71cb1eff0426eec
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-23 11:03:59 +0100

    Spostate le librerie app.js e vendor.js in fondo al body

--- resources/views/split/master.blade.php
+++ resources/views/split/master.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/pace-theme-loading-bar.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ elixir('js/vendor.js') }}"></script>
     <script>window.Laravel = <?= json_encode(['csrfToken' => csrf_token()]); ?></script>
     <style>
         
@@ -74,7 +72,8 @@
     </div>
 </div>
 
-
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ elixir('js/vendor.js') }}"></script>
     <script src="{{asset('js/lang/i18n.js')}}"></script>
     @stack('jsfooter')
     <script>
