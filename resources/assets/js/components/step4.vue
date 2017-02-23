134fd965814dd253005c6141ef5a904384449f4e
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-02-23 09:33:58 +0100

    WIP: Gestione dividers in tabs, completata, per ora niente img ma solo checkbox

--- resources/assets/js/components/step4.vue
+++ resources/assets/js/components/step4.vue
@@ -6,13 +6,6 @@
         <div class="col-lg-12" style="min-height: 300px">
             AREA RISERVATA AL 2D
         </div>
-        <!--
-        <div class="col-lg-3" v-for="divider in $parent.dividers">
-            <div class="checkbox">
-                <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}} x {{divider.depth}})</label>
-            </div>
-        </div>
-        -->
         <div class="col-lg-12">
             <ul class="nav nav-tabs">
                 <li  :class="{active: !index}" v-for="(cat,index) in $parent.dividers.dividersCategories">
@@ -20,27 +13,13 @@
                 </li>
             </ul>
             <div class="tab-content">
-                <div id="elem445" class="tab-pane fade in active">
-                    <div class="col-lg-3" v-for="divider in $parent.dividers.dividers">
-                        <div class="checkbox">
-                            <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}} x {{divider.depth}})</label>
-                        </div>
+                <div :class="{active: !index}" :id="'elem'+cat" class="tab-pane fade in" v-for="(cat,index) in $parent.dividers.dividersCategories">
+                    <div class="checkbox" v-for="divider in $parent.getDividerByCat(cat)">
+                        <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}})</label>
                     </div>
                 </div>
-
-                <div id="elem705" class="tab-pane fade in ">
-                    <div class="col-lg-3" v-for="divider in $parent.dividers">
-                        <div class="checkbox">
-                            <label><input type="checkbox" value="" :id="divider.id" @click="$parent.pushDivider( $event )">({{divider.width}} x {{divider.length}} x {{divider.depth}})</label>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
-
-
-
         <div class="col-lg-12" >
             <a href="#step5" class="btn btn-danger inpagenav" lang="it">Avanti</a>
         </div>
