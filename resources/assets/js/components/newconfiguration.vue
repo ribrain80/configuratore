a08ef7b97872a75e6ba32bd121a7fef838ffc6b3
Brambilla Riccardo <BrambillaRi@valueteam.com>
2017-02-24 15:59:32 +0100

    step 2 button check added - explicit add done

---
+++ resources/assets/js/components/newconfiguration.vue
@@ -0,0 +1,5 @@
+<template>
+	<ul class="nav navbar-nav">
+	    <li><a lang="it" href='#' data-toggle="modal" data-target="#new-conf-modal">Nuova configurazione</a></li>
+	</ul>
+</template>
---
+++ resources/assets/js/split/newconfiguration.js
@@ -0,0 +1,23 @@
+/**
+ * Gestione del package template newconfiguration
+ * @type {Vue}
+ */
+var newconfiguration = new Vue({
+
+    el: 'newconfiguration',
+
+    data: {
+        
+    },
+
+    methods: {
+       
+        pagereload: function () {
+            
+        }
+    },
+
+    mounted() {
+        console.log( "new conf component initialized" );
+    }
+});
