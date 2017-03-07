4ff9b5ca418d8eada5187fc2659d165bb23617d9
vagrant <vagrant@homestead>
2017-03-07 14:57:49 +0000

    gallery initial import

--- gulpfile.js
+++ gulpfile.js
@@ -16,11 +16,13 @@ require('laravel-elixir-vue-2');
 
 var paths = {
     'jquery': './vendor/bower_components/jQuery/dist/',
-    'jqueryui': './vendor/bower_components/jquery-ui/',
+    // 'jqueryui': './vendor/bower_components/jquery-ui/',
     'bootstrap': './vendor/bower_components/bootstrap-sass-official/assets/',
     'lang': './vendor/bower_components/jquery-lang-js/js/',
     'two': './vendor/bower_components/two.js/build/',
-    'pace': './vendor/bower_components/PACE/'
+    'pace': './vendor/bower_components/PACE/',
+    'lightgallery': './vendor/bower_components/lightgallery/dist/',
+    'lightgallerythumb': './vendor/bower_components/lg-thumbnail/dist/'
 };
 
 elixir(function(mix) {
@@ -31,15 +33,20 @@ elixir(function(mix) {
     mix.copy('./resources/assets/js/split/**', 'public/js/split' );
     mix.copy('./resources/images', 'public/images');
     mix.copy( paths.pace + 'themes/black/**', 'public/css');
+    mix.copy( paths.lightgallery + 'css/lightgallery.min.css', 'public/css');
+    mix.copy( paths.lightgallery + 'img/**', 'public/img');
+    mix.copy( paths.lightgallery + 'fonts/**', 'public/fonts');
 
     mix.sass("app.scss", 'public/css/')
         .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
         .scripts([
-            paths.jqueryui + "jquery-ui.min.js",
+            // paths.jqueryui + "jquery-ui.min.js",
             paths.lang + "js.cookie.js",
             paths.lang + "jquery-lang.js",
             paths.two + "two.js",
-            paths.pace + "pace.min.js"
+            paths.pace + "pace.min.js",
+            paths.lightgallery + "js/lightgallery.min.js",
+            paths.lightgallerythumb + "lg-thumbnail.min.js",
         ], 'public/js/vendor.js', './');
 
 });
