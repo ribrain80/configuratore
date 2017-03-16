480c2b2695fd05838173056b810ad3e0f83a743f
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-03-16 10:04:23 +0100

    Estetic fixes to ExportController so Riccardo is Happy

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -14,35 +14,67 @@ use LynX39\LaraPdfMerger\PdfManage;
 use PDF;
 use App;
 
+/**
+ * This controller handle all the export requests
+ * Class ExportController
+ * @package App\Http\Controllers
+ */
 class ExportController extends Controller
 {
+    const FIRST_PAGE_ITEMS_NUMBER = 4;
+    const OTHER_PAGES_ITEMS_NUMBER = 6;
+    
+    /**
+     * Return a pdf file for a drawer with a starting brochure if needed
+     * @param $id
+     * @param bool $brochure
+     * @param string $language
+     * @return mixed
+     */
     public function actionRiepilogo($id,$brochure=false,$language='it') {
+        //Set application locale from the request param language
         app()->setLocale($language);
+        
+        //Load the Drawer from db, if throw an exception if the drawer dont exist
         $model = App\Models\Drawer::with(['drawertype','edgecolor','drawerbridges','drawerdividers'])->findOrFail($id);
-        $dividers = $this->handleDividers($model->drawerdividers,$model->drawerbridges);
+        
+        //Handle all kind of dividers,bridges and supports
+        $elements = $this->handleElements($model->drawerdividers,$model->drawerbridges);
 
+        //Get a Pdf builder instance
         $drawerPdf = App::make('snappy.pdf.wrapper');
+        //Set the route for the default page header 
         $drawerPdf->setOption('header-html',route('split.pdf.header',['drawer'=>$id],true));
-        $drawerPdf->loadView('split.pdf.riepilogo', ['model'=>$model,'dividers'=>$dividers]);
-
-
-
+        //Load a view into the Pdf Builder
+        $drawerPdf->loadView('split.pdf.riepilogo', ['model'=>$model,'dividers'=>$elements]);
+        
+        //If the request parameter brochure is false just return the pdf
         if (!$brochure) {
             return $drawerPdf->inline();
         }
-        $openingFile = resource_path('pdf/brochure.pdf' );
+        
+        //Otherwise get an Instance of PdfManage for concat brochure and generated pdf
         $pdf = new PdfManage();
-        $pdf->addPDF($openingFile);
-        //SALVO TEMPORANEAMENTE IL FILE
+        
+        //Add the bruchure to the resulting pdf
+        $pdf->addPDF(resource_path('pdf/brochure.pdf' ));
+        
+        //Get an unique temp file for save the drawer pdf
         $temp = tempnam(sys_get_temp_dir(), 'drawer_'.$id);
+        
+        //Save the generated pdf in a temp file
         $drawerPdf->save($temp,true);
-        //FINE SALVATAGGIO
+        
+        //Add the saved drawer pdf to the result
         $pdf->addPDF($temp);
+        
+        //Merge brochure and drawer pdf then return
         $pdf->merge();
     }
 
+    
     /**
-     * Azione che genera l'html per la costruzione del pdf
+     * It generate the header for all pdf pages.
      * @param $drawer
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -52,7 +84,15 @@ class ExportController extends Controller
     }
 
 
-    private function handleDividers(Collection $dividers,Collection $bridges) {
+    /**
+     * Interna function that reduce Dividers, Bridges and Supports in a more usefull structure
+     * When needed it pad the pages with null objects
+     * @todo Refactor for group items by sku
+     * @param Collection $dividers
+     * @param Collection $bridges
+     * @return array
+     */
+    private function handleElements(Collection $dividers,Collection $bridges) {
         $total = [];
         foreach ($dividers as $cur) {
             $total[]=$cur;
@@ -64,22 +104,22 @@ class ExportController extends Controller
         $pages = [];
         $cont=0;
         foreach ($total as $elem) {
-            if ($cont < 4) {
+            if ($cont < self::FIRST_PAGE_ITEMS_NUMBER) {
                 $first[]=$elem;
                 $cont++;
             } else {
                 $pages[]=$elem;
             }
         }
-        $pages = array_chunk($pages,6);
+        $pages = array_chunk($pages,self::OTHER_PAGES_ITEMS_NUMBER);
 
         //GESTIONE PAD CON null
 
-        $first = array_pad($first,4,null);
+        $first = array_pad($first,self::FIRST_PAGE_ITEMS_NUMBER,null);
 
         if (count($pages)) {
             $tmp = array_pop($pages);
-            $tmp = array_pad($tmp,6,null);
+            $tmp = array_pad($tmp,self::OTHER_PAGES_ITEMS_NUMBER,null);
             $pages[]=$tmp;
         }
 
