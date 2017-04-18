9887a034b7375743dbce6c6f55857904cc100474
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-04-18 17:15:41 +0200

    Fix on camera aspect ratio

--- resources/assets/js/3d/components/camera.js
+++ resources/assets/js/3d/components/camera.js
@@ -8,10 +8,7 @@ export default class Camera {
         const height = renderer.domElement.height;
 
         // Create and position a Perspective Camera
-        this.threeCamera = new THREE.PerspectiveCamera(Config.camera.fov, 1, Config.camera.near, Config.camera.far);
-
-        //this.threeCamera = new THREE.OrthographicCamera (0.5 * 600 / - 2, 0.5 * 600 / 2, 600 / 2, 600 / - 2, 150, 1000);
-
+        this.threeCamera = new THREE.PerspectiveCamera(Config.camera.fov, width / height, Config.camera.near, Config.camera.far);
         this.threeCamera.position.set(Config.camera.posX, Config.camera.posY, Config.camera.posZ);
 
         // Initial sizing
@@ -19,11 +16,9 @@ export default class Camera {
 
         // Listeners
         window.addEventListener('resize', () => this.updateSize(renderer), false);
-        document.getElementById("step4_3d_container").addEventListener('resize', () => this.updateSize(renderer), false);
     }
 
     updateSize(renderer) {
-        console.log("Update camera");
         // Multiply by dpr in case it is retina device
         this.threeCamera.aspect = renderer.domElement.width * Config.dpr / renderer.domElement.height * Config.dpr;
 
