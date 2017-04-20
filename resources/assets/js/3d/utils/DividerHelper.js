c02f2f00c9e35d4300db55ad3b587baa8360c14a
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-04-20 18:25:24 +0200

    Little comments on DividerHelper

--- resources/assets/js/3d/utils/DividerHelper.js
+++ resources/assets/js/3d/utils/DividerHelper.js
@@ -25,34 +25,57 @@ export default class DividerHelper {
      * @param coords
      */
     addDivider (name,model,textureImg,coords) {
-        console.log("TEXTURE IMG:", textureImg);
+
         this.objLoader.loadModel(name,model,textureImg,coords).then((obj3d) => {
             this.scene.add(obj3d);
         });
 
     }
 
+    /**
+     * Remove an object from the scene
+     * @param name
+     */
     removeDivider(name) {
         let _objToRemove = this.scene.getObjectByName( name, true );
-        this.scene.remove( _objToRemove );
+
+        if (_objToRemove) {
+            this.scene.remove( _objToRemove );
+        }
     }
 
+    /**
+     * Change a divider Position
+     * @param name
+     * @param x
+     * @param z
+     */
     updateDividerPosition(name,x,z) {
         let _obj = this.scene.getObjectByName( name, true );
-        console.log("UPDATE 3D POS",name,_obj);
+
         if (_obj) {
             _obj.position.set( x, 0, z )
         }
     }
 
+    /**
+     * Update Divider texture.
+     *
+     * In real the function create a new model and remove the old one keeping the same position
+     * @param name
+     * @param model
+     * @param textureImg
+     */
     updateDividerTexture(name,model,textureImg) {
+
         let _obj = this.scene.getObjectByName( name, true );
-        console.log("UPDATE TEXTURE!!!",name,model,textureImg,_obj);
+
         let _coords = {
             x:_obj.position.x,
             y:_obj.position.y,
             z:_obj.position.z,
         };
+
         this.objLoader.loadModel(name,model,textureImg,_coords).then((obj3d) => {
             this.scene.add(obj3d);
             this.scene.remove(_obj);
