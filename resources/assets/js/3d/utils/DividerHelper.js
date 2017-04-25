41db35da986f0dfdb4ad6730d9104e65ee733c03
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-04-25 12:09:33 +0200

    3d aggiunti modelli lineabox. manca il lineabox 4 lati

--- resources/assets/js/3d/utils/DividerHelper.js
+++ resources/assets/js/3d/utils/DividerHelper.js
@@ -100,84 +100,35 @@ export default class DividerHelper {
 
 
     genDrawer(type,w,l) {
+
+        if (!type) {
+            return;
+        }
+
+        let zDeltaCorrection = -21;
+        let yDeltaCorrection = -35;
         //let drawer = false;
         if (type ==4) {
-
-
-
             let backgroundCoords = {x:w/2,y:-30,z:l};
             let sxCoords = {x:0,y:-30,z:l};
             let dxCoords = {x:w,y:-30,z:l};
 
-
-
-
-
             // # start loading and placing the background
             // # ugly hack ... i dont know why i need a texture at the first init
-
             this.objLoader.loadModel("background",'/images/3dmodels/legno/background.obj','http://homestead.app/images/textures/02_Acero.jpg').then((obj3d) => {
                 // # Change background dimension
                 let bbox = new THREE.Box3().setFromObject( obj3d );
                 obj3d.scale.set(Math.abs(w / (bbox.max.x - bbox.min.x)),1,Math.abs(l / (bbox.max.z - bbox.min.z)));
                 obj3d.updateMatrix();
                 obj3d.position.x = w/2;
-                obj3d.position.y = -30;
-                obj3d.position.z = l -21;
+                obj3d.position.y = yDeltaCorrection;
+                obj3d.position.z = l + zDeltaCorrection;
                 obj3d.updateMatrix();
                 // # Add background to the scene
                 this.drawer.add(obj3d);
             });
-/*
-            // # load and place the front
-            this.objLoader.loadModel("front",'/images/3dmodels/legno/front.obj','http://homestead.app/images/textures/02_Acero.jpg').then((front) => {
-                // # Change background dimension
-                let bbox = new THREE.Box3().setFromObject( front );
-                let elZ = (bbox.max.z - bbox.min.z);
-                front.scale.set(Math.abs(w / (bbox.max.x - bbox.min.x)),1,1);
-                front.position.x = w/2;
-                front.position.z = -elZ/2;
-                front.position.y = -30;
-                // # Add background to the scene
-                this.scene.add(front);
-            });
-
-            // # load and place the back
-            this.objLoader.loadModel("back",'/images/3dmodels/legno/back.obj','http://homestead.app/images/textures/02_Acero.jpg').then((back) => {
-                // # Change background dimension
-                let bbox = new THREE.Box3().setFromObject( back );
-                back.scale.set(Math.abs(w / (bbox.max.x - bbox.min.x)),1,1);
-                let elZ = (bbox.max.z - bbox.min.z);
-                back.position.x = w/2;
-                back.position.z = 2*l-elZ/2;
-                back.position.y = -30;
-
-                console.log("BACK ELZ:",elZ);
-                // # Add background to the scene
-                this.scene.add(back);
-            });
-
-            // # load and place the back
-            this.objLoader.loadModel("left",'/images/3dmodels/legno/sx.obj','http://homestead.app/images/textures/02_Acero.jpg').then((left) => {
-                // # Change background dimension
-                let bbox = new THREE.Box3().setFromObject( left );
-                left.scale.set(1,1,1);
-                left.position.x = 0;
-                left.position.y = -30;
-                left.position.z = l;
-                // # Add background to the scene
-                this.scene.add(left);
-            });
-
-            // # load and place the back
-            this.objLoader.loadModel("right",'/images/3dmodels/legno/dx.obj','http://homestead.app/images/textures/02_Acero.jpg',dxCoords).then((obj3d) => {
-                // # Change background dimension
-                let bbox = new THREE.Box3().setFromObject( obj3d );
-                obj3d.scale.set(1,1,1);
-                // # Add background to the scene
-                this.scene.add(obj3d);
-            });
-*/
+        } else {
+            // # Here the lineabox Drawers
         }
     }
 
