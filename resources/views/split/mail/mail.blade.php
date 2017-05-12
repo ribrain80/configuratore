c24a2d66731cab4bde31529c4866cf1c8007f689
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-05-12 14:33:33 +0200

    Aggiunto testo mail

--- resources/views/split/mail/mail.blade.php
+++ resources/views/split/mail/mail.blade.php
@@ -1,5 +1,9 @@
 <html>
 <body>
-Test di invio email con allegato per split conf
+Grazie per aver utilizzato il configuratore Split.<br>
+In allegato la documentazione relativa alla sua composizione.<br>
+
+Arturo Salice Spa<br>
+<a href="http://www.salice.com">www.salice.com</a>
 </body>
 </html>
