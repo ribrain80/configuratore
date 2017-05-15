adc00d7a5ac5a4ee82d7b654565a0e7c2cb76319
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-05-15 20:29:44 +0200

    PDF: Label in francese

---
+++ resources/lang/fr/pdf.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'header_type'=>'TYPE<br/>DE TIROIR',
+    'header_edges' => 'FINITION<br/>CÔTÉS',
+    'header_back' => 'FINITION<br/>PANNEAU ARRIÈRE',
+    'header_background' => 'FINITION<br/>FOND',
+    'header_front' => 'FINITION<br/>FAÇADE',
+    'header_internalWidth'=>'Profondeur intérieure tiroir',
+    'header_internalDepth'=>'Largeur intérieure tiroir',
+    'header_internalHeight'=>'Hauteur intérieure tiroir',
+    'nr'=>'NR',
+    'code'=>'CODE ARTICLE',
+    'dimensions'=>'DIMENSIONS',
+    'finish'=>'COULEUR/TEXTURE',
+    'label_contenitore'=>'BAC DE RANGEMENT',
+    'label_elemento_ponte'=>'BAC DE RANGEMENT TRANSVERSAUX',
+    'label_supporto'=>'SUPPORTO',
+];
