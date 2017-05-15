87a653a4e4429f7f4c09941174b4a423578630ff
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-05-15 20:24:37 +0200

    PDF: Label in portoghese

---
+++ resources/lang/pt/pdf.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'header_type'=>'TIPO<br/>DE GAVETA',
+    'header_edges' => ' ACABAMENTO<br/>LATERAIS',
+    'header_back' => ' ACABAMENTO<br/> TRASEIRA',
+    'header_background' => ' ACABAMENTO<br/>FUNDO',
+    'header_front' => ' ACABAMENTO<br/>FRENTE',
+    'header_internalWidth'=>'Profundidade interna do gaveta',
+    'header_internalDepth'=>'Largura interna do gaveta',
+    'header_internalHeight'=>'Altura interna do gaveta',
+    'nr'=>'NR',
+    'code'=>'CÓDIGO',
+    'dimensions'=>'DIMENSÕES',
+    'finish'=>'COR/ACABAMENTO TÁTIL',
+    'label_contenitore'=>'RECIPIENTE',
+    'label_elemento_ponte'=>'ELEMENTO PONTE',
+    'label_supporto'=>'SUPPORTO',
+];
