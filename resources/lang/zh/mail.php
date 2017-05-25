48d1626a18d00e49e4d48a84c520ba221f45f065
Giuseppe Liberati <giuseppe.liberati@dservicemedia.it>
2017-05-25 09:13:15 +0200

    Traduzioni zh per pdf

---
+++ resources/lang/zh/mail.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'subject'=>'Salice Split - 配置清单',
+    'body_line_1' => '感谢您使用 Split 配件器。',
+    'body_line_2' => '附件为您的应用配置信息。',
+];
---
+++ resources/lang/zh/pdf.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+return [
+    'header_type'=>'抽屉类型',
+    'header_edges' => '侧帮颜色',
+    'header_back' => '背板颜色',
+    'header_background' => '底板颜色',
+    'header_front' => '面板颜色',
+    'header_internalWidth'=>'抽屉内部深度',
+    'header_internalDepth'=>'抽屉内部宽度',
+    'header_internalHeight'=>'抽屉内部高度',
+    'nr'=>'数量',
+    'code'=>'货号',
+    'dimensions'=>'尺寸',
+    'finish'=>'表面',
+    'label_contenitore'=>'存储单元',
+    'label_elemento_ponte'=>'搁架单元',
+    'label_supporto'=>'搁架支撑轨',
+];
